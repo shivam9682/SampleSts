82fe4f96b1ed97cc767660df0c47036745f69a78
shivam9682 <ssivamyadav0123@gmail.com>
2026-05-04 13:46:04 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -79,8 +79,7 @@ useEffect(() => {
     localStorage.removeItem('admin');
     localStorage.removeItem('adminEmail');
     localStorage.removeItem('isAuth');
- localStorage.removeItem('accessToken');
-  localStorage.removeItem('refreshToken');
+ 
     setUser(null);
     setAdmin(null);
 
